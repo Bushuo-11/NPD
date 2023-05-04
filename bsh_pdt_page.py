86793da968b6d8039e9af5ccef0eb1303759cb8d
Bushuo-11 <106621162+Bushuo-11@users.noreply.github.com>
2023-05-04 22:15:55 +0800

    Update bsh_pdt_page.py

--- bsh_pdt_page.py
+++ bsh_pdt_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 import plotly.graph_objects as go
 import requests
 from urllib.parse import quote,unquote
